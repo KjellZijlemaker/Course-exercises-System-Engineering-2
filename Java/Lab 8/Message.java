e4d338422909f744a3bae46334b4a76fb4b75f3a
Kjell Zijlemaker <kjell.zijlemaker@hva.nl>
2014-10-12 15:27:03 +0200

    Added extra comments

--- Java/Lab 8/Message.java
+++ Java/Lab 8/Message.java
@@ -1,17 +1,36 @@
-
+/**
+ * Java bean for getters and setters, for the message passing
+ * 
+ * @author kjellzijlemaker
+ *
+ */
 public class Message {
-	 private String msg;
-	 
-	    public Message(String str){
-	        this.msg=str;
-	    }
-	   
-	    
-	    public String getMsg() {
-	        return this.msg;
-	    }
-	 
-	    public void setMsg(String str) {
-	        this.msg=str;
-	    }
+	private String msg;
+
+	/**
+	 * Constructor for giving message content
+	 * 
+	 * @param str
+	 */
+	public Message(String str) {
+		this.msg = str;
+	}
+
+	/**
+	 * Getting the message content
+	 * 
+	 * @return
+	 */
+	public String getMsg() {
+		return this.msg;
+	}
+
+	/**
+	 * Setting the message content
+	 * 
+	 * @param str
+	 */
+	public void setMsg(String str) {
+		this.msg = str;
+	}
 }
